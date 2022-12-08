5fde01fa7acfc27e8dec3539d91d3de5d12bab59
John <hattonjd@iu.edu>
2022-12-08 18:38:48 -0500

    Add FourBitRAM Class to Project

---
+++ source/Memory/FourBitRAM.cpp
@@ -0,0 +1,9 @@
+//
+// Created by snake on 12/8/2022.
+//
+
+#include "FourBitRAM.h"
+
+FourBitRAM::FourBitRAM() {
+
+}
---
+++ source/Memory/FourBitRAM.h
@@ -0,0 +1,18 @@
+//
+// Created by snake on 12/8/2022.
+//
+#pragma once
+
+
+
+
+class FourBitRAM {
+
+
+private:
+
+public:
+
+    FourBitRAM();
+
+};
